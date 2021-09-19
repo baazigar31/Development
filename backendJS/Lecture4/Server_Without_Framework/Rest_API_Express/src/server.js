268c4c3fb896ef39af3f726af44dce38c648db95
baazigar31 <imanand731@gmail.com>
2021-09-19 19:31:24 +0530

    Express Routing

--- backendJS/Lecture4/Server_Without_Framework/Rest_API_Express/src/server.js
+++ backendJS/Lecture4/Server_Without_Framework/Rest_API_Express/src/server.js
@@ -3,22 +3,70 @@ import morgan from "morgan";
 import { json, urlencoded } from "body-parser";
 
 const app = express();
+const router=express.Router();
 
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
+// app.use(json());
+// app.use(urlencoded({extended : true}));
 
 
-app.get('/', (req, res) => {
+//This is a custom middleware
+const customLogger=(req,res,next)=>{
+    console.log("Hi, Shubham How are you? ");
+    next();
+}
+
+app.use('/api/v1',router);
+
+// Her to pass more than one middleware we can pass array in it.
+app.get('/',customLogger, (req, res) => {
     console.log(req.body);
-    res.send({message: "OK"});
+    res.send({message: "OK GET"});
 });
 
-app.post('/post', (req, res) => {
+app.post('/', (req, res) => {
     console.log(req.body);
     res.send({message: "OK POST"});
 });
+
+
+// Here code redundancy exist for post and get request to avoid this chaining is done.
+// router.get('/post',(req,res)=>{
+//     res.send({message : "Router OK"})
+// });
+
+// router.post('/post',(req,res)=>{
+//     res.send({message : "Router OK"})
+// });
+
+
+//THROUGH this chaining we can reduce the code redundancy.
+router
+    .route('/post')
+    .get((req,res)=>{
+            console.log("This is a GET request");
+            res.send({message : "Router OK"})
+    })
+    .post((req,res)=>{
+        res.send({message : "Router OK"})
+    });  
+
+router
+    .route('/post/:id')
+    .get((req,res)=>{
+        console.log("This is a GET request and the id is ",req.params);
+        res.send({message : "Router OK"})
+    })
+    .post((req,res)=>{
+        console.log("This is a POST request and the id is ",req.params);
+        res.send({message : "Router OK"})
+    });     
+
 
 export const start = () => {
     app.listen(3000, () => {
         console.log("Server started at 3000");
     });
 }
+
+// console.log("Heyyy shubham anand");
